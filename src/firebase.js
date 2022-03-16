489411686cd1f49c0a68a86c40d0a9c8d8b4001c
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-03-16 16:16:53 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD5QBpSIDuWuBMTV5Xn6TzNqp_pLF8aroc',
@@ -12,4 +12,5 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
